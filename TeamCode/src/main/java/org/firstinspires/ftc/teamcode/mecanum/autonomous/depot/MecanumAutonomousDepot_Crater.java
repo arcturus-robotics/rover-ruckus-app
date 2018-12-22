aef00d4665837bf3dc13e32eca110add22ab5f5a
SenorBunBun <33770177+SenorBunBun@users.noreply.github.com>
2018-12-22 11:56:51 -0500

    Update MecanumAutonomousDepot_Crater

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanum/autonomous/depot/MecanumAutonomousDepot_Crater.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanum/autonomous/depot/MecanumAutonomousDepot_Crater.java
@@ -18,13 +18,16 @@ public class MecanumAutonomousDepot_Crater extends MecanumAutonomousDepot {
 
         if (opModeIsActive()) {
             // Move to depot
+            moveLeft(0.7, 220);
             moveForward(1, (Mecanum.LANDER / 3));
             moveLeft(1, Mecanum.LANDER / 2);
             turnRight(1, Mecanum.CIRCLE / 12);
-            moveBackward(1, (long)(Mecanum.FIELD * 0.4));
-            turnClaw(1, 500);
-            turnClaw(-1, 500);
-            turnRight(0.7, 300);
+            moveBackward(1, (long)(Mecanum.FIELD * 0.2));
+            moveRight(0.7, 370);
+            moveBackward(1, (long)(Mecanum.FIELD*0.2));
+            turnClaw(0.7, 300);
+            turnClaw(-0.7, 400);
+            turnLeft(0.7, 220);
             moveForward(1, (long) (Mecanum.FIELD * 0.5));
         }
     }
