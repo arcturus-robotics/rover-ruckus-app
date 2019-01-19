8f436b7e15dec6a423eaeef7a34cf5166cde4c3b
Robogoogol <29505620+Robogoogol@users.noreply.github.com>
2019-01-19 11:35:44 -0500

    Reimplement arm and intake for RobotTeleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/teleop/RobotTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/teleop/RobotTeleop.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.robot.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.robot.RobotOpMode;
@@ -11,9 +12,6 @@ import org.firstinspires.ftc.teamcode.robot.RobotOpMode;
 @TeleOp(name = "Robot Teleop", group = "Robot Teleop")
 //@Disabled
 public class RobotTeleop extends RobotOpMode {
-    public static final double ARM_POWER = 0.5;
-    public static final double CLAW_POWER = 0.5;
-
     @Override
     public void loop() {
         float frontLeft = Range.clip(-gamepad1.left_stick_y + gamepad1.left_stick_x, -1, 1);
@@ -35,23 +33,16 @@ public class RobotTeleop extends RobotOpMode {
             robot.latch.setPower(0);
         }
 
-        /* Unimplemented
         // Arm
         if (gamepad2.left_bumper) {
-            robot.armTilt.setPower(-ARM_POWER);
+            robot.armLeft.setPower(-1);
+            robot.armRight.setPower(-1);
         } else if (gamepad2.right_bumper) {
-            robot.armTilt.setPower(ARM_POWER);
+            robot.armLeft.setPower(1);
+            robot.armRight.setPower(1);
         } else {
-            robot.armTilt.setPower(0);
-        }
-
-        // Claw
-        if (gamepad2.left_trigger >= BUTTON_THRESHOLD) {
-            robot.clawTilt.setPower(-CLAW_POWER);
-        } else if (gamepad2.right_trigger == BUTTON_THRESHOLD) {
-            robot.clawTilt.setPower(CLAW_POWER);
-        } else {
-            robot.clawTilt.setPower(0);
+            robot.armLeft.setPower(0);
+            robot.armRight.setPower(0);
         }
 
         // Intake
@@ -67,6 +58,5 @@ public class RobotTeleop extends RobotOpMode {
             robot.leftIntake.setPosition(1);
             robot.rightIntake.setPosition(1);
         }
-        */
     }
 }
