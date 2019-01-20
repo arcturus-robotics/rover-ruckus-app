a11961d87d5febd499ae8696449a6a182c85c190
Robogoogol <29505620+Robogoogol@users.noreply.github.com>
2019-01-20 14:30:22 -0500

    Make the motor turn rate test turn the front left drive motor instead

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/measurement/MotorTurnRateMeasurement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/measurement/MotorTurnRateMeasurement.java
@@ -13,16 +13,14 @@ import org.firstinspires.ftc.teamcode.Constants;
 @Autonomous(name = "Motor Turn Rate", group = "Measurement Tests")
 //
 public class MotorTurnRateMeasurement extends RobotLinearOpMode {
-    protected TestMotorHardware robot = new TestMotorHardware();
-
     @Override
     public void runOpMode() {
         super.runOpMode();
 
-        robot.testMotor.setPower(1);
+        robot.frontLeftDrive.setPower(1);
 
         sleep(1000);
 
-        robot.testMotor.setPower(0);
+        robot.frontLeftDrive.setPower(0);
     }
 }
