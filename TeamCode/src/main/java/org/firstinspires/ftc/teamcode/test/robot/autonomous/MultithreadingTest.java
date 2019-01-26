135f983b4dbf0ce5af846cdec471448c9eda1a7b
Robogoogol <29505620+Robogoogol@users.noreply.github.com>
2019-01-26 12:52:56 -0500

    Update multithreading test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/robot/autonomous/MultithreadingTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/robot/autonomous/MultithreadingTest.java
@@ -6,11 +6,15 @@ import org.firstinspires.ftc.teamcode.robot.RobotLinearOpMode;
 public class MultithreadingTest extends RobotLinearOpMode {
     @Override
     public void runOpMode() {
-        LatchThread latchThread = new LatchThread();
-        DriveThread driveThread = new DriveThread();
+        super.runOpMode();
 
-        latchThread.run();
-        driveThread.run();
+        if (opModeIsActive()) {
+            LatchThread latchThread = new LatchThread();
+            DriveThread driveThread = new DriveThread();
+
+            latchThread.run();
+            driveThread.run();
+        }
     }
 
     private class LatchThread extends Thread {
